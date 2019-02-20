0d070b46c78964b8d194b4ba2056b0bf15a730ac
DanilsGrics <kamukaq3e@gmail.com>
2019-02-20 19:42:02 +0200

    Validation solvation

--- src/com/javaguru/shoppinglist/service/validation/ProductNameValidationRule.java
+++ src/com/javaguru/shoppinglist/service/validation/ProductNameValidationRule.java
@@ -7,10 +7,6 @@ public class ProductNameValidationRule implements ProductValidationRule {
     @Override
     public void validate(Product product) {
         checkNotNull(product);
-        if (product.getName() == null) {
-            throw new ProductValidationException("Product name must be not null");
-        }
-
         if (product.getName().length() < 3 || product.getName().length() > 32) {
             throw new ProductValidationException("Name must contain 3 - 32 symbols");
         }
